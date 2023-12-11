8eb45ce1dc3c79b04acab2a07eda6b2958d9d2f5
Annie Wilson <annemarie.wilson@yahoo.com>
2023-12-11 22:37:21 +0000

    completed NewEntryForm.js with handleFormSubmit and handleChange functions

--- client/src/forms/NewEntryForm.js
+++ client/src/forms/NewEntryForm.js
@@ -20,11 +20,47 @@ const NewEntryForm = ({ postNewEntry }) => {
                 moodRating: "",
             }
         );
+        console.log("Journal entry posted successfuly!");
     }
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setNewEntry((prevEntry) => ({
+          ...prevEntry,
+          [name]: name === "moodRating" ? value.toUpperCase() : value,
+        }));
+      };
 
     return (
         <>
-
+         <form id="new-journal-entry" onSubmit={handleFormSubmit}>
+      <label>
+        Content:
+        <textarea
+          name="content"
+          value={newEntry.content}
+          onChange={handleInputChange}
+        />
+      </label>
+      <label>
+        Weekday:
+        <input
+          type="text"
+          name="weekDay"
+          value={newEntry.weekDay}
+          onChange={handleInputChange}
+        />
+      </label>
+      <label>
+        Mood Rating:
+        <input
+          type="text"
+          name="moodRating"
+          value={newEntry.moodRating}
+          onChange={handleInputChange}
+        />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
         </>
     );
 }
