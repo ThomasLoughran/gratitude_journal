fead758cd95559951364b3e2cf9297656c8e91ae
Thomas Loughran <tloughran3@gmail.com>
2023-12-11 15:30:27 +0000

    added post method to add a new journal entry

--- client/src/containers/JournalContainer.js
+++ client/src/containers/JournalContainer.js
@@ -10,7 +10,6 @@ const JournalContainer = () => {
       const response = await fetch(`http://localhost:8080/users/${id}`);
       const data = await response.json();
       setCurrentUser(data);
-      console.log(data);
     } catch (error) {
       console.error("help", error);
     }
@@ -20,16 +19,35 @@ const JournalContainer = () => {
     try {
       const response = await fetch(`http://localhost:8080/journal-entries/${id}/all`);
       const data = await response.json();
-      console.log(data);
       setJournalEntries(data);
+      console.log("get all response",data)
     } catch (error){
       console.error("error fetching entries", error);
     }
   }
 
+  const postNewEntry = async (newEntry, userId) => {
+    const response = await fetch(`http://localhost:8080/journal-entries/${userId}`, {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(newEntry)
+    });
+    console.log("post mapping journal entries", journalEntries);
+    setJournalEntries([...journalEntries, newEntry]);
+  }
+
+  const newPostObject = {
+    content: "This is a test",
+    weekDay: "FRIDAY",
+    moodRating: "REALLYGOOD"
+  }
+
+
   useEffect(() => {
     fetchUserById(1);
     fetchAllEntriesByUserId(2);
+    postNewEntry(newPostObject, 2);
+
   }, []);
   return (
     <>
