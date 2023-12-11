d7fa657b867b0e9690368225f3db64ce5b0af57a
Janet_Mensah <janetmensah60@gmail.com>
2023-12-11 11:42:49 +0000

    Created a function to fetch User data by ID

--- client/src/containers/JournalContainer.js
+++ client/src/containers/JournalContainer.js
@@ -2,17 +2,28 @@ import { useState, useEffect } from "react";
 import JournalList from "../components/JournalList";
 
 const JournalContainer = () => {
+  const [journalEntries, setJournalEntries] = useState([]);
+  const [currentUser, setCurrentUser] = useState(null); //come back to edit after Anna
 
-    const [journalEntries, setJournalEntries] = useState([]);
-    const [currentUser, setCurrentUser] = useState(null); //come back to edit after Anna
-    
-    return ( 
-        <>
-        <h1>Gratitude Journal</h1>
-         <JournalList journals={journalEntries}/>
-        </>
+  const fetchUserById = async (id) => {
+    try {
+      const response = await fetch(`http://localhost:8080/users/${id}`);
+      const data = await response.json();
+      setCurrentUser(data);
+      console.log(data);
+    } catch (error) {
+      console.error("help", error);
+    }
+  };
+  useEffect(() => {
+    fetchUserById(1);
+  }, []);
+  return (
+    <>
+      <h1>Gratitude Journal</h1>
+      <JournalList journals={journalEntries} />
+    </>
+  );
+};
 
-     );
-}
- 
 export default JournalContainer;
