6358cffedd3972e01afdbfab87153a11bb411faf
Hezekiah Adeusi <hezekiaha58@gmail.com>
2023-12-13 13:57:56 +0000

    Implemented react-pro-router syntax to NavBar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,6 +1,7 @@
 import { Link, Outlet } from "react-router-dom";
 import { useContext } from "react";
 import { UserContext } from "../containers/JournalContainer";
+import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 
 const NavBar = () => {
   const { currentUser } = useContext(UserContext) || {};
@@ -17,43 +18,76 @@ const NavBar = () => {
 
   return (
     <>
-      <nav>
-        <div className="navbar-container">
+      <Sidebar>
+        <Menu
+          menuItemStyles={{
+            button: {
+              // the active class will be added automatically by react router
+              // so we can use it to style the active menu item
+              [`&.active`]: {
+                backgroundColor: '#13395e',
+                color: '#b6c8d9',
+              },
+            },
+          }}
+        >
+          <MenuItem component={<Link to="/" />}> Home</MenuItem>
+          <MenuItem component={<Link to="/entries" />}> My Entries</MenuItem>
+          <MenuItem component={<Link to="/entries/new" />}> Create New Journal Entry</MenuItem>
+          <MenuItem
+            component={<Link to="/entries/new">
+                currentUser && (
+                  <li>
+                    <button onClick={handleLogout}>
+                      <Link to="/">Sign Out</Link>
+                    </button>
+                  </li>
+                )
+            </Link>}>
 
-          <div className="logo">
-            <Link to="/">Gratitude Journal</Link>
-          </div>
-
-          <div className="menu">
-            <label htmlFor="menu-toggle">&#9776; Menu</label>
-
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-
-              <li>
-                <Link to="/entries">My Entries</Link>
-              </li>
-
-              <li>
-                <Link to="/entries/new">Create New Entry</Link>
-              </li>
-
-              {currentUser && (
-                <li>
-                  <button onClick={handleLogout}>
-                    <Link to="/">Sign Out</Link>
-                  </button>
-                </li>
-              )}
-            </ul>
-          </div>
-        </div>
-      </nav>
-      <Outlet />
+          </MenuItem>
+        </Menu>
+      </Sidebar>
     </>
   );
-};
+}
 
 export default NavBar;
+//   return (
+//     <>
+//       <nav className="navbar-container">
+
+//         <div className="logo">
+//           <Link to="/">Gratitude Journal</Link>
+//         </div>
+
+//         <div className="menu">
+//           <label htmlFor="menu-toggle">&#9776; Menu</label>
+
+//           <ul>
+//             <li>
+//               <Link to="/">Home</Link>
+//             </li>
+
+//             <li>
+//               <Link to="/entries">My Entries</Link>
+//             </li>
+
+//             <li>
+//               <Link to="/entries/new">Create New Entry</Link>
+//             </li>
+
+//             {currentUser && (
+//               <li>
+//                 <button onClick={handleLogout}>
+//                   <Link to="/">Sign Out</Link>
+//                 </button>
+//               </li>
+//             )}
+//           </ul>
+//         </div>
+//       </nav>
+//       <Outlet />
+//     </>
+//   );
+// };
