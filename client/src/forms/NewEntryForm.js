2974127e6149ef8001eb47ecc83ce14d2fdd0768
Thomas Loughran <tloughran3@gmail.com>
2023-12-13 20:42:14 +0000

    new post form now has dropdown lists for weekday and mood

--- client/src/forms/NewEntryForm.js
+++ client/src/forms/NewEntryForm.js
@@ -15,17 +15,31 @@ const NewEntryForm = ({ submitForm }) => {
         }
     );
 
+  const weekdays = ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY'];
+  const weekdayOptions = weekdays.map((weekday) => {
+    return <option value={weekday}>{weekday}</option>
+  })
+
+  const moods = ['REALLYBAD', 'NEGATIVE', 'INDIFFERENT', 'POSITIVE', 'REALLYGOOD'];
+  const moodOptions = moods.map((mood) => {
+    return <option value={mood}>{mood}</option>
+  })
+
     const handleFormSubmit = (event) => {
         event.preventDefault();
-        submitForm(newEntry, 2); // remove dis line in d future
-        setNewEntry(
-            {
-                content: "",
-                weekDay: "",
-                moodRating: "",
-            }
-        );
-        console.log("Journal entry posted successfully!");
+        if (newEntry.content === "" || newEntry.moodRating=== "" || newEntry.weekDay === ""){
+          return alert("Incomplete form")
+        } else {
+          submitForm(newEntry, 2); // remove dis line in d future
+          setNewEntry(
+              {
+                  content: "",
+                  weekDay: "",
+                  moodRating: "",
+              }
+          );
+          console.log("Journal entry posted successfully!");
+        }
     }
     const handleInputChange = (event) => {
         const { name, value } = event.target;
@@ -36,37 +50,33 @@ const NewEntryForm = ({ submitForm }) => {
       };
 
     return (
-        <>
-         <form id="new-journal-entry" onSubmit={handleFormSubmit}>
-      <label>
-        Content:
-        <textarea
-          name="content"
-          value={newEntry.content}
-          onChange={handleInputChange}
-        />
-      </label>
-      <label>
-        Weekday:
-        <input
-          type="text"
-          name="weekDay"
-          value={newEntry.weekDay}
-          onChange={handleInputChange}
-        />
-      </label>
-      <label>
-        Mood Rating:
-        <input
-          type="text"
-          name="moodRating"
-          value={newEntry.moodRating}
-          onChange={handleInputChange}
-        />
-      </label>
-      <button type="submit">Submit</button>
-    </form>
-        </>
+        <form id="new-journal-entry" onSubmit={handleFormSubmit}>
+          <label>Content:</label>
+            <textarea
+              name="content"
+              value={newEntry.content}
+              onChange={handleInputChange}
+            />
+          <label>Mood</label>
+          <select 
+              name="moodRating"
+              defaultValue="select-mood"
+              onChange={handleInputChange}
+              >
+              <option disabled value="select-mood">Mood</option>
+              {moodOptions}    
+          </select>
+          <label>Weekday</label>
+              <select 
+                  name="weekDay"
+                  defaultValue="select-weekday"
+                  onChange={handleInputChange}
+                  >
+                  <option disabled value="select-weekday">Weekday</option>
+                  {weekdayOptions}
+              </select>
+          <button type="submit">Submit</button>
+        </form>
     );
 }
 
