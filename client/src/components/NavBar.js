1648fb9c26ce890e983b475d3425f5d6e8fdc514
Hezekiah Adeusi <hezekiaha58@gmail.com>
2023-12-14 09:28:20 +0000

    Cleaned up NavBar.js file

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,7 +2,7 @@ import { Link, Outlet } from "react-router-dom";
 import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../containers/JournalContainer";
 import { Sidebar, Menu, MenuItem, SidebarProps } from 'react-pro-sidebar';
-import {HomeOutlinedIcon} from "@mui/material"
+import { HomeOutlinedIcon } from "@mui/material"
 import { FiHome } from "react-icons/fi";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { FaPlus } from "react-icons/fa";
@@ -10,13 +10,11 @@ import { IoIosJournal } from "react-icons/io";
 import { CiLogin } from "react-icons/ci";
 import { CiLogout } from "react-icons/ci";
 
-
-
 const NavBar = ({ setJournalEntries }) => {
 
   const [collapsed, setCollapsed] = useState(true);
-  
-  const handleToggleSidebar = () => { 
+
+  const handleToggleSidebar = () => {
     setCollapsed(!collapsed);
   }
 
@@ -44,17 +42,16 @@ const NavBar = ({ setJournalEntries }) => {
 
   return (
     <>
-      <Sidebar 
-      className="sidebar"
-      style={({width: collapsed ? '60px': '900px', display: "flex"})}
-      collapsed={collapsed}>
-        <RxHamburgerMenu onClick={handleToggleSidebar}
-        style={({marginLeft:'30px'})}
-        ></RxHamburgerMenu>
-        <Menu
-          // className="sidebar"
-          // style={({width: collapsed ? '60px': '300px', display: "flex", background:'#E1E8ED'})}
+      <Sidebar
+        className="sidebar"
+        style={({ width: collapsed ? '60px' : '900px', display: "flex" })}
+        collapsed={collapsed}>
+        <RxHamburgerMenu
+          onClick={handleToggleSidebar}
+          style={({ marginLeft: '30px' })}>
+        </RxHamburgerMenu>
 
+        <Menu
           menuItemStyles={{
             button: {
               // the active class will be added automatically by react router
@@ -67,21 +64,25 @@ const NavBar = ({ setJournalEntries }) => {
           }}
         >
 
-          <MenuItem 
-          //  style={{ width: collapsed ? '60px': '12000px'}}
-          icon={<FiHome />}>Home</MenuItem>
+          <MenuItem icon={<FiHome />}>Home</MenuItem>
+
           <MenuItem
-          //  style={{ width: collapsed ? '60px': '12000px'}}
             icon={<IoIosJournal />}
-            component={<Link to="/entries" />}> My Entries</MenuItem>
+            component={<Link to="/entries" />}>
+            My Entries
+          </MenuItem>
+
           <MenuItem
-          //  style={{ width: collapsed ? '60px': '12000px'}}
             icon={<FaPlus />}
-            component={<Link to="/entries/new" />}>Create New Journal Entry</MenuItem>
+            component={<Link to="/entries/new" />}>
+            Create New Journal Entry
+          </MenuItem>
+
           <MenuItem
-          //  style={{ width: collapsed ? '60px': '12000px'}}
             icon={<CiLogin />}
-            component={<Link to="/sign-in" />}> Sign In</MenuItem>
+            component={<Link to="/sign-in" />}>
+            Sign In
+          </MenuItem>
           {renderSignOut()}
 
         </Menu>
@@ -92,42 +93,3 @@ const NavBar = ({ setJournalEntries }) => {
 }
 
 export default NavBar;
-
-//   return (
-//     <>
-//       <nav className="navbar-container">
-
-//         <div className="logo">
-//           <Link to="/">Gratitude Journal</Link>
-//         </div>
-
-//         <div className="menu">
-//           <label htmlFor="menu-toggle">&#9776; Menu</label>
-
-//           <ul>
-//             <li>
-//               <Link to="/">Home</Link>
-//             </li>
-
-//             <li>
-//               <Link to="/entries">My Entries</Link>
-//             </li>
-
-//             <li>
-//               <Link to="/entries/new">Create New Entry</Link>
-//             </li>
-
-//             {currentUser && (
-//               <li>
-//                 <button onClick={handleLogout}>
-//                   <Link to="/">Sign Out</Link>
-//                 </button>
-//               </li>
-//             )}
-//           </ul>
-//         </div>
-//       </nav>
-//       <Outlet />
-//     </>
-//   );
-// };
