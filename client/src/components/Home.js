f836717097ac9c79ea6ef0bf0de6d6b60022c3c9
Annie Wilson <annemarie.wilson@yahoo.com>
2023-12-15 09:46:33 +0000

    updated welcome message and font

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,14 +1,19 @@
 import { Link, Outlet } from "react-router-dom";
 
 const Home = () => {
-    return (
-        <>
-            <article>
-            <h2>Welcome to The Gratitude Journal</h2>
-            <h3><strong>Open your journal, open your heart to gratitude!</strong></h3>
-            </article>
-        </>
-    );
-}
+  return (
+    <>
+      <article>
+        <h2>Welcome to The Gratitude Journal</h2>
+        <p>
+          A warm welcome from the Positivity Pathfinders, where it's our mission
+          to map the route to a grateful life! This is a space for you to
+          express your thoughts, feelings, and experiences. Use the sidebar to
+          navigate through different sections and manage your journal entries.
+        </p>
+      </article>
+    </>
+  );
+};
 
 export default Home;
